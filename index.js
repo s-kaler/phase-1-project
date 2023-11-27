3231903a13a02fc1c0a239934f92c38c62aa753c
Sundeep <sundeep.kaler@gmail.com>
2023-11-27 07:09:56 -0500

    set up form reading function

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-let quizGenerated = false;
-
 const init = () => {
+    let quizGenerated = false;
     let dbURL = ""
+    let form = document.querySelector('#generate-form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let qNum = e.target["q-num"].value;
+        let difficulty = e.target.difficulty.value;
+        let category = e.target["trivia-category"].value;
+        if(qNum === ''){
+            alert('Please enter a number')
+        }
+        else{
+            buildQuiz(qNum, difficulty, category)
+            form.reset();
+        }
+    });
 }
 
-document.addEventListener("DOMContentLoaded", init)
+function buildQuiz(qNum, difficulty, category) {
+   
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", init);
